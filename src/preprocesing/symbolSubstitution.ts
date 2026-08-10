8de6b125f6e63c8b66041a80a4457284dfb8aa3d
BOTTINI Thomas <thomas.bottini@cnrs.fr>
2026-08-10 15:54:46 +0200

    📡

--- src/preprocesing/symbolSubstitution.ts
+++ src/preprocesing/symbolSubstitution.ts
@@ -1,17 +1,16 @@
 export default function process(input: string): string {
 	let lines = input.split(/\r?\n/);
 	const symbolsDict: Record<string, string> = {};
+	const r: RegExp = /(\p{Extended_Pictographic})=(.*)/u;
 
 	for (const line of lines) {
-		const symbolMatch = line.match(/(\p{Extended_Pictographic})=(.*)/u);
+		const symbolMatch = line.match(r);
 		if (symbolMatch) {
 			symbolsDict[symbolMatch[1]] = symbolMatch[2];
 		}
 	}
 
-	lines = lines.filter(
-		(line) => line.match(/(\p{Extended_Pictographic})=(.*)/u) === null,
-	);
+	lines = lines.filter((line) => line.match(r) === null);
 
 	for (const [symbol, symbolValue] of Object.entries(symbolsDict)) {
 		for (let i = 0; i < lines.length; i++) {
