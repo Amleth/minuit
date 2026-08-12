856715959dc10e5122b4a9cf7b872e43e2095f21
BOTTINI Thomas <thomas.bottini@cnrs.fr>
2026-08-12 21:30:57 +0200

    📡

--- src/structuralScanner/scanPatternValue.ts
+++ src/structuralScanner/scanPatternValue.ts
@@ -1,3 +1,7 @@
-import { PatternItem } from "./PatternItem.ts";
+import type PatternItem from "./patternItems/PatternItem.ts";
 
-export default function (input: string): PatternItem[] {}
+export default function (input: string): PatternItem[] {
+	const patternItems: PatternItem[] = [];
+
+	return patternItems;
+}
